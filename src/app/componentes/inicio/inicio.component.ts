258c6a953f2bc9122e8ca74f5b4df24e3e78150c
Cuautrix <hptablet098@gmail.com>
2024-06-18 12:50:37 -0600

    v.0.4.1

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-
-@Component({
-  selector: 'app-inicio',
-  standalone: true,
-  imports: [],
-  templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.scss'
-=======
 import { BannerComponent } from "../banner/banner.component";
 import { MenuComponent } from "../menu/menu.component";
 import { BodyComponent } from "../body/body.component";
+
 
 @Component({
     selector: 'app-inicio',
@@ -18,7 +10,6 @@ import { BodyComponent } from "../body/body.component";
     templateUrl: './inicio.component.html',
     styleUrl: './inicio.component.scss',
     imports: [BannerComponent, MenuComponent, BodyComponent]
->>>>>>> v.0.4.0
 })
 export class InicioComponent {
 
