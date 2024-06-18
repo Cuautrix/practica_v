81e17fcaf74f1e36e6e26d2a4d45c5b188f1499f
Cuautrix <hptablet098@gmail.com>
2024-06-18 12:15:17 -0600

    v.0.3.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'inicio',component:InicioComponent
+    },
+    {
+        path:'**',component:InicioComponent,
+    }
+];
--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from "../banner/banner.component";
+import { MenuComponent } from "../menu/menu.component";
+import { BodyComponent } from "../body/body.component";
 
 @Component({
-  selector: 'app-inicio',
-  standalone: true,
-  imports: [],
-  templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.scss'
+    selector: 'app-inicio',
+    standalone: true,
+    templateUrl: './inicio.component.html',
+    styleUrl: './inicio.component.scss',
+    imports: [BannerComponent, MenuComponent, BodyComponent]
 })
 export class InicioComponent {
 
